7fd0b777dd5956ebb0be493dd821408d492acd9e
viampietro <vincent.iampietro@outlook.fr>
2017-04-27 12:39:32 +0200

    Ajout du fichier lemmatizer.py, tous les mots sont remplaces par leur lemme dans data/lemmatized_data.csv

--- tree-tagger/pos-tag-processing-verb.py
+++ tree-tagger/pos-tag-processing-verb.py
@@ -2,10 +2,8 @@ import re
 import subprocess
 
 data_set_file = open("../data/dataset.csv", "r");
-pos_tag_file = open("../data/pos_tag_verb.csv", "a");
+pos_tag_file = open("../data/lemmatized_data.csv", "a");
 
-# liste des tags qui valident un mot (le mot est retenu lorsqu'il est tagge par au moins un de ces tags) RB = negation
-tags_list = ['JJ', 'JJR', 'JJS', 'VB', 'VBG', 'VBD','VBN','VBZ', 'VBP', 'RB'] 
 
 # lecture de chaque document contenu dans dataset.csv (un document = un commentaire)
 for data_line in data_set_file :
@@ -24,22 +22,20 @@ for data_line in data_set_file :
     
     # lecture de chaque ligne de tree_tagger_stdout
     for tree_tagger_line in tree_tagger_stdout.split('\n') :
-    
+        
         # si la ligne eclatee en suivant les tabs contient 3 elements (MOT, TAG et LEMME)
         # on recupere le tag dans grammatical_category
         if (len(tree_tagger_line.split('\t')) == 3) :
             word = tree_tagger_line.split('\t')[0]
-            grammatical_category = tree_tagger_line.split('\t')[1]
             lemma = tree_tagger_line.split('\t')[2]
 
             # si la categorie du mot est une des categories eligibles, contenue dans tags_list
             # on ecrit le mot dans le fichier pos_tag_verb.csv
-            if (grammatical_category in tags_list) :
-                if (lemma != "<unknown>") :
-                    filtered_document += lemma + " "
-                else :
-                    filtered_document += word + " "
-                    
+            if (lemma != "<unknown>") :
+                filtered_document += lemma + " "
+            else :
+                filtered_document += word + " "
+                
     pos_tag_file.write(filtered_document + "\n")
     pos_tag_file.flush()    
     
