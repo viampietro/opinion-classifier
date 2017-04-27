2e6a84c3a3e3efd81ff28863adb4641de9d5d00e
viampietro <vincent.iampietro@outlook.fr>
2017-04-27 09:03:26 +0200

    Ajout du fichier pos_tag_ver.arff et pos_tag_verb.csv dans data

--- tree-tagger/pos-tag-processing-verb.py
+++ tree-tagger/pos-tag-processing-verb.py
@@ -2,7 +2,7 @@ import re
 import subprocess
 
 data_set_file = open("../data/dataset.csv", "r");
-pos_tag_file = open("../data/pos_tag.csv", "a");
+pos_tag_file = open("../data/pos_tag_verb.csv", "a");
 
 # liste des tags qui valident un mot (le mot est retenu lorsqu'il est tagge par au moins un de ces tags) RB = negation
 tags_list = ['JJ', 'JJR', 'JJS', 'VB', 'VBG', 'VBD','VBN','VBZ', 'VBP', 'RB'] 
@@ -24,20 +24,25 @@ for data_line in data_set_file :
     
     # lecture de chaque ligne de tree_tagger_stdout
     for tree_tagger_line in tree_tagger_stdout.split('\n') :
-
+    
         # si la ligne eclatee en suivant les tabs contient 3 elements (MOT, TAG et LEMME)
         # on recupere le tag dans grammatical_category
         if (len(tree_tagger_line.split('\t')) == 3) :
+            word = tree_tagger_line.split('\t')[0]
             grammatical_category = tree_tagger_line.split('\t')[1]
+            lemma = tree_tagger_line.split('\t')[2]
 
             # si la categorie du mot est une des categories eligibles, contenue dans tags_list
-            # on ecrit le mot dans le fichier pos_tag.csv
+            # on ecrit le mot dans le fichier pos_tag_verb.csv
             if (grammatical_category in tags_list) :
-                filtered_document += tree_tagger_line.split()[2] + " "
-                
+                if (lemma != "<unknown>") :
+                    filtered_document += lemma + " "
+                else :
+                    filtered_document += word + " "
+                    
     pos_tag_file.write(filtered_document + "\n")
-        
-        
+    pos_tag_file.flush()    
+    
     
     
     
