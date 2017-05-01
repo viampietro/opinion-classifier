cd44a08da2915276b7a83c0cb7406cca328a1264
viampietro <vincent.iampietro@outlook.fr>
2017-05-01 09:24:08 +0200

    Modification de la fonction tag_file dans file-tagger.py, utilisation du module python treetaggerwrapper. Renommage des fichiers dans data pour ne garder que les fichiers utiles. Ajout dans les fichiers arff de train_data_imdb et train_data_imdb_lemmatized qui contiennent tout le corpus imdb de 25000 instances. Je pense que les 10000 docs qu'on avait au début était tirés de ces 25000 instances.

--- scripts/file-tagger.py
+++ scripts/file-tagger.py
@@ -1,11 +1,23 @@
 import re
 import subprocess
+import pprint
+import treetaggerwrapper
 
+####################### !!!! POUR FAIRE MARCHER TAG_FILE !!!! #########################
 #
-# PRE-REQUIS :
-# 1. se placer dans le dossier du data_file specifie.
-# 2. data_file doit etre une fichier texte.
-# 3. le dossier bin et cmd du tree-tagger doivent etre ajoutes au PATH
+# Il faut installer le wrapper python pour tree-tagger :
+#
+# 1. Installer le module treetaggerwrapper :
+#    (il peut etre necessaire d'installer le module setuptools : pip install setuptools)
+#    pip install treetaggerwrapper OU pip3 install treetaggerwrapper
+#
+# 2. Mettre le dossier treetagger contenant les executables dans votre dossier /home
+# pour que le module treetaggerwrapper le detecte
+# ex : /home/treetagger
+########################################################################################
+
+########################################################################################
+# PRE-REQUIS : source_file doit etre une fichier texte.
 #
 # PARAM :
 # @data_file, le fichier source a tagger.
@@ -22,37 +34,34 @@ import subprocess
 # ['JJ', 'JJR', 'JJS'], pour ne garder que les adjectifs 
 #  
 
-def tag_file (data_file, target_tagged_file, tags_list=[]) :
+def tag_file (source_file_name, destination_file_name, tags_list=[]) :
 
-    data_set_file = open(data_file + ".csv", "r");
-    tagged_file = open(target_tagged_file + ".csv", "a");
+    # objet TreeTagger
+    tagger = treetaggerwrapper.TreeTagger(TAGLANG='en')
+
+    source_file = open(source_file_name + ".csv", "r");
+    destination_file = open(destination_file_name + ".csv", "a");
     
 
     # lecture de chaque document contenu dans dataset.csv (un document = un commentaire)
-    for data_line in data_set_file :
+    for document in source_file :
 
-        # envoi de la sortie de echo dans l'entree de tree-tager-english
-        echo_stdout = subprocess.Popen(('echo', data_line), stdout=subprocess.PIPE)
-
-        # tree_tagger_stdout est compose de lignes de la forme "MOT \t TAG \t LEMME \n"
-        tree_tagger_stdout = subprocess.run(['tree-tagger-english', ''],
-                                            stdin=echo_stdout.stdout,
-                                            stdout=subprocess.PIPE).stdout.decode('utf-8')
+        tagger_output = tagger.tag_text(document)
         
         # variable qui contient le document filtre par tree-tagger avant insertion
         # dans le fichier pos_tag.csv
         filtered_document = ""
 
         # lecture de chaque ligne de tree_tagger_stdout
-        for tree_tagger_line in tree_tagger_stdout.split('\n') :
+        for tag in tagger_output :
         
             # si la ligne eclatee en suivant les tabs contient 3 elements (MOT, TAG et LEMME)
             # on recupere le tag dans grammatical_category
-            if (len(tree_tagger_line.split('\t')) == 3) :
+            if (len(tag.split('\t')) == 3) :
 
-                word = tree_tagger_line.split('\t')[0]
-                grammatical_category = tree_tagger_line.split('\t')[1]
-                lemma = tree_tagger_line.split('\t')[2]
+                word = tag.split('\t')[0]
+                grammatical_category = tag.split('\t')[1]
+                lemma = tag.split('\t')[2]
 
                 # si tags_list est vide ou si le mot a une categorie grammaticale eligible
                 if (not tags_list) or (grammatical_category in tags_list) :
@@ -63,8 +72,8 @@ def tag_file (data_file, target_tagged_file, tags_list=[]) :
                     else :
                         filtered_document += word + " "
                     
-        tagged_file.write(filtered_document + "\n")
-        tagged_file.flush()    
+        destination_file.write(filtered_document + "\n")
+        destination_file.flush()    
 
         
     
